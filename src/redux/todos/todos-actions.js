29faba57d0e180a84fb45cf59e54b84005a4231e
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-03-29 20:58:39 +0300

    Redux

--- src/redux/todos/todos-actions.js
+++ src/redux/todos/todos-actions.js
@@ -1,7 +1,7 @@
 import types from './todos-types';
 import { v4 as uuidv4 } from 'uuid';
 
-const addContacts = (name, number) => {
+const addContacts = ({ name, number }) => {
   return {
     type: types.ADD,
     payload: {
